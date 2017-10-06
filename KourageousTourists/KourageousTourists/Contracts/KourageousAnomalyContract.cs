4d57f645b96f7e36e8571fb569f2e4b04542acf4
whale2 <whale2.box@gmail.com>
2017-10-06 20:59:14 +0200

    Added Anomaly Contracts.

---
+++ KourageousTourists/KourageousTourists/Contracts/KourageousAnomalyContract.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Contracts;
+using FinePrint.Contracts.Parameters;
+
+namespace KourageousTourists
+{
+
+	public class KourageousAnomaly
+	{
+		public string name { get; set; }
+		public string anomalyDescription { get; set; }
+		public CelestialBody body { get; set; }
+		public string contractDescription { get; set; }
+		public string contractSynopsis { get; set; }
+		public string contractCompletion { get; set; }
+		public float payoutModifier;
+
+		public void Save(ConfigNode node) {
+			node.AddValue ("anomaly", body.name + ":" + name);
+		}
+
+		public static KourageousAnomaly Load(ConfigNode node, Dictionary<String, KourageousAnomaly> anomalies) {
+			string anomalyName = node.GetValue ("anomaly");
+			return (KourageousAnomaly)anomalies [anomalyName].MemberwiseClone ();
+		}
+	}
+
+	public class KourageousAnomalyContract : KourageousContract
+	{
+		public const string anomalyCfgNode = "ANOMALY";
+		public const string anomalyDistance = "anomalyDiscoveryDistance";
+		internal static Dictionary<String, KourageousAnomaly> anomalies = null;
+		protected KourageousAnomaly chosenAnomaly;
+		protected static float anomalyDiscoveryDistance = 50.0f;
+
+		public KourageousAnomalyContract () : base () {}
+
+		internal static void readAnomalyConfig() {
+
+			if (anomalies != null)
+				return;
+			
+			anomalies = new Dictionary<String, KourageousAnomaly> ();
+			ConfigNode config = GameDatabase.Instance.GetConfigNodes(
+				KourageousTouristsAddOn.cfgRoot).FirstOrDefault();
+			if (config == null)
+				return;
+
+
+			String distanceNode = config.GetValue (anomalyDistance);
+			if (distanceNode != null) {
+				try {
+					anomalyDiscoveryDistance = (float)Convert.ToDouble(distanceNode);
+				}
+				catch(Exception) {
+				}
+			}
+
+			ConfigNode[] nodes = config.GetNodes (anomalyCfgNode);
+			foreach (ConfigNode node in nodes) {
+
+				KourageousAnomaly anomaly = new KourageousAnomaly ();
+
+				KourageousTouristsAddOn.printDebug (String.Format ("cfg node: {0}", node));
+				String name = node.GetValue("name");
+				if (name == null)
+					continue;
+				anomaly.name = name;
+
+				KourageousTouristsAddOn.printDebug (String.Format ("anomaly name: {0}", name));
+				String anomalyDescription = node.GetValue ("anomalyDescription");
+				if (anomalyDescription == null)
+					continue;
+				anomaly.anomalyDescription = anomalyDescription;
+
+				String contractDescription = node.GetValue ("contractDescription");
+				if (contractDescription == null)
+					continue;
+				anomaly.contractDescription = contractDescription;
+
+				String contractSynopsis = node.GetValue ("contractSynopsis");
+				if (contractSynopsis == null)
+					continue;
+				anomaly.contractSynopsis = contractSynopsis;
+
+				String bodyStr = node.GetValue ("body");
+				KourageousTouristsAddOn.printDebug (String.Format ("anomaly body: {0}", bodyStr));
+
+				foreach (CelestialBody b in FlightGlobals.Bodies) {
+					KourageousTouristsAddOn.printDebug (String.Format ("list body name: {0}", b.name));
+					if (b.name.Equals (bodyStr)) {
+						anomaly.body = b;
+						break;
+					}
+				}
+				KourageousTouristsAddOn.printDebug (String.Format ("anomaly body obj: {0}", anomaly.body == null));
+				if (anomaly.body == null)
+					continue;
+				
+				String payoutModifierStr = node.GetValue ("payoutModifier");
+				KourageousTouristsAddOn.printDebug (String.Format ("payout modifier str: {0}", payoutModifierStr));
+				if (payoutModifierStr == null)
+					continue;
+				float payoutModifier = 1.0f;
+				try {
+					payoutModifier = (float)Convert.ToDouble(payoutModifierStr);
+					KourageousTouristsAddOn.printDebug (String.Format ("payout modifier: {0}", payoutModifier));
+				}
+				catch(Exception) {
+				}
+				anomaly.payoutModifier = payoutModifier;
+
+				anomalies.Add (bodyStr + ":" + name, anomaly);
+				KourageousTouristsAddOn.printDebug (String.Format ("added: {0}", bodyStr + ":" + name));
+			}
+			
+		}
+
+		protected KourageousAnomaly chooseAnomaly(CelestialBody body) {
+
+			KourageousTouristsAddOn.printDebug ("entered");
+			readAnomalyConfig ();
+			KourageousTouristsAddOn.printDebug (String.Format("anomalies: {0}, distance: {1}", 
+				anomalies.Count, anomalyDiscoveryDistance));
+
+			List<KourageousAnomaly> chosen = new List<KourageousAnomaly> ();
+			foreach (KeyValuePair<string, KourageousAnomaly> entry in anomalies)
+				if (entry.Value.body.name.Equals (body.name))
+					chosen.Add (entry.Value);
+
+			KourageousTouristsAddOn.printDebug (String.Format("chosen: {0}, cnt: {1}", 
+				chosen, chosen.Count));
+			if (chosen.Count == 0)
+				return null;
+
+			Random rnd = new Random ();
+			return chosen [rnd.Next (chosen.Count)];
+		}
+
+
+		protected override bool Generate()
+			//System.Type contractType, Contract.ContractPrestige difficulty, int seed, State state)
+		{
+			KourageousTouristsAddOn.printDebug ("entered");
+
+			targetBody = selectNextCelestialBody ();
+			if (targetBody == null)
+				return false;
+
+			chosenAnomaly = chooseAnomaly (targetBody);
+			if (chosenAnomaly == null)
+				return false;
+			
+			this.numTourists = UnityEngine.Random.Range (2, 5);
+			KourageousTouristsAddOn.printDebug ("num tourists: " + numTourists);
+			for (int i = 0; i < this.numTourists; i++) {
+				ProtoCrewMember tourist = CrewGenerator.RandomCrewMemberPrototype (ProtoCrewMember.KerbalType.Tourist);
+
+				this.tourists.Add (tourist);
+				KourageousTouristsAddOn.printDebug ("generated: " + tourist.name);
+
+				// TODO: Add support for gender for 1.3 build
+				KerbalTourParameter itinerary = new KerbalTourParameter (tourist.name, tourist.gender);
+				// TODO: Add difficulty multiplier
+				itinerary.FundsCompletion = 25000.0;
+				itinerary.ReputationCompletion = 0.0f;
+				itinerary.ReputationFailure = 0.0f;
+				itinerary.ScienceCompletion = 0.0f;
+				this.AddParameter (itinerary);
+
+				KerbalDestinationParameter dstParameter = new KerbalDestinationParameter (
+					targetBody, FlightLog.EntryType.Land, tourist.name
+				);
+				dstParameter.FundsCompletion = 1000.0f;
+				dstParameter.FundsFailure = 0.0f;
+				dstParameter.ReputationCompletion = 0.0f;
+				dstParameter.ReputationFailure = 0.0f;
+				dstParameter.ScienceCompletion = 0.0f;
+				/*dstParameter.NestToParent (itinerary);
+				dstParameter.CreateID ();
+				dstParameter.AddParameter (new Contracts.Parameters.LandOnBody (targetBody));*/
+				itinerary.AddParameter (dstParameter);
+
+
+				KourageousAnomalyParameter anomalyParameter = new KourageousAnomalyParameter (
+					targetBody, tourist.name, chosenAnomaly.name, chosenAnomaly.anomalyDescription
+				);
+				anomalyParameter.FundsCompletion = 1300.0;
+				anomalyParameter.FundsFailure = 0.0;
+				anomalyParameter.ReputationCompletion = 1.0f;
+				anomalyParameter.ReputationFailure = 1.0f;
+				anomalyParameter.ScienceCompletion = 0.0f;
+				itinerary.AddParameter (anomalyParameter);
+			}
+
+			GenerateHashString ();
+
+			base.SetExpiry ();
+			base.SetScience (0.0f, targetBody);
+			base.SetDeadlineYears (1, targetBody);
+			base.SetReputation (2, 5, targetBody);
+			base.SetFunds (
+				3000 * chosenAnomaly.payoutModifier, 
+				9000 * chosenAnomaly.payoutModifier, 
+				21000 * chosenAnomaly.payoutModifier, 
+				targetBody);
+
+			return true;
+		}
+
+		protected override void OnAccepted() {
+			KourageousTouristsAddOn.printDebug ("entered: body=" + targetBody.bodyName);
+			foreach (ProtoCrewMember tourist in tourists) {
+				HighLogic.CurrentGame.CrewRoster.AddCrewMember (tourist);
+				KourageousTouristsAddOn.printDebug ("adding to roster: " + tourist.name);
+			}
+		}
+
+
+		public override bool CanBeCancelled() {
+			// TODO: Let's make that if any tourist is out of Kerbin, 
+			// the contract can't be cancelled
+			return true;
+		}
+
+		public override bool CanBeDeclined() {
+			return true;
+		}
+
+		protected override void GenerateHashString() {
+			string hash = "selfiecntrct-" + targetBody.bodyName;
+			foreach (ProtoCrewMember tourist in this.tourists)
+				hash += tourist.name;
+			this.hashString = hash;
+		}
+
+		protected override string GetTitle () {
+			KourageousTouristsAddOn.printDebug ("entered: anomaly=" + chosenAnomaly);
+			return String.Format("Visit {0} with {1}",
+				chosenAnomaly.anomalyDescription,  getProperTouristWordLc());
+		}
+
+		protected override string GetDescription() {
+			return KourageousContract.tokenize (
+				chosenAnomaly.contractDescription, getProperTouristWord(), anomalyDiscoveryDistance);
+		}
+
+		protected override string GetSynopsys() {
+			return KourageousContract.tokenize (
+				chosenAnomaly.contractSynopsis, getProperTouristWordLc(), anomalyDiscoveryDistance);
+		}
+
+		protected override string MessageCompleted ()
+		{
+			return KourageousContract.tokenize (chosenAnomaly.contractCompletion,
+				getProperTouristWordLc (), anomalyDiscoveryDistance);
+		}
+
+		public override bool MeetRequirements ()
+		{
+			// Later we should offer the contract only after some other tourist contract were completed
+			return true;
+		}
+
+		protected override void OnSave (ConfigNode node) {
+			base.OnSave (node);
+			chosenAnomaly.Save(node);
+		}
+
+		protected override void OnLoad(ConfigNode node) {
+			base.OnLoad (node);
+			readAnomalyConfig ();
+			chosenAnomaly = KourageousAnomaly.Load (node, anomalies);
+		}
+	}
+}
+
---
+++ KourageousTourists/KourageousTourists/Contracts/KourageousAnomalyParameter.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Linq;
+using Contracts;
+using UnityEngine;
+
+namespace KourageousTourists
+{
+	public class KourageousAnomalyParameter: KourageousParameter 
+	{
+		public const string discoveryDistance = "anomalyDiscoveryDistance";
+		public const float defaultMinAnomalyDistance = 50;
+
+		protected string anomalyName;
+		protected string anomalyDisplayName;
+		protected float minAnomalyDistance;
+
+
+		public KourageousAnomalyParameter() : base() {}
+
+		public KourageousAnomalyParameter(
+			CelestialBody target, string kerbal, string anomaly, string displayName) : base(target, kerbal) {
+
+			this.anomalyName = anomaly;
+			this.anomalyDisplayName = displayName;
+
+			setDistance ();
+		}
+
+		protected void setDistance() {
+			ConfigNode config = GameDatabase.Instance.GetConfigNodes(
+				KourageousTouristsAddOn.cfgRoot).FirstOrDefault();
+			this.minAnomalyDistance = defaultMinAnomalyDistance;
+			if (config != null) {
+
+				String dscvr = config.GetValue (discoveryDistance);
+				if (dscvr != null) {
+					try {
+						this.minAnomalyDistance = (float)Convert.ToDouble (dscvr);
+					} catch (Exception) {
+					}
+				}
+			}
+			else
+				KourageousTouristsAddOn.printDebug ("no config found in game database");
+		}
+
+		protected override string GetHashString() {
+			return "walk" + this.targetBody.bodyName + this.tourist;
+		}
+
+		protected override string GetTitle() {
+			return String.Format ("Take photo of {0} from the surface of {1} near the {2}",
+				tourist, targetBody.bodyName, anomalyDisplayName);
+		}
+
+		protected override string GetMessageComplete() {
+			return String.Format ("{0} was pictured on the surface of {1} in the vicinity of {2}",
+				tourist, targetBody.bodyName, anomalyDisplayName);
+		}
+
+		protected override void OnRegister() {
+			KourageousTouristsAddOn.selfieListeners.Add (onSelfieTaken);
+		}
+
+		protected override void OnUnregister() {
+			KourageousTouristsAddOn.selfieListeners.Remove (onSelfieTaken);
+		}
+
+		private void onSelfieTaken() {
+
+			KourageousTouristsAddOn.printDebug ("here");
+			foreach (Vessel v in FlightGlobals.VesselsLoaded) {
+				if (
+					v.mainBody == targetBody &&
+					v.GetVesselCrew () [0].name.Equals (tourist) &&
+					v.GetVesselCrew().Count == 1 &&
+					v.situation == Vessel.Situations.LANDED && v.isEVA) {
+
+					KourageousTouristsAddOn.printDebug ("checking for " + tourist + " at " + anomalyName);
+					if (this.isNearbyAnomaly (v, anomalyName)) {
+						base.SetComplete ();
+					}
+					break;
+				}
+			}
+		}
+
+		private bool isNearbyAnomaly(Vessel v, string anomalyName) {
+
+
+			// FIXME: Can we have objects with same names, but on different bodies?
+			// FIXME: So far I think we can.
+			GameObject[] obj = UnityEngine.Object.FindObjectsOfType<GameObject>();
+			foreach (GameObject anomalyObj in obj) {
+				
+				Component[] c = anomalyObj.GetComponents<PQSCity> ();
+				if (c == null || c.Length == 0)
+					continue;
+				KourageousTouristsAddOn.printDebug ("has pqscity: " + anomalyObj.name);
+				PQSCity pqscity = (PQSCity)c [0];
+				if (pqscity == null)
+					continue;
+
+				if (!pqscity.sphere.isAlive)
+					continue;
+
+				Transform tr = anomalyObj.GetComponent<Transform> ();
+
+				if (!anomalyObj.name.Equals (anomalyName))
+					continue;
+				if (tr == null)
+					return false;
+				float dist1 = Vector3.Distance (v.transform.position, tr.position);
+				KourageousTouristsAddOn.printDebug ("distance: " + dist1.ToString() + 
+					"; min dist: " + minAnomalyDistance.ToString());
+				if (dist1 < this.minAnomalyDistance)
+					return true;
+			}
+			return false;
+		}
+	
+		protected override void OnLoad (ConfigNode node)
+		{
+			base.OnLoad (node);
+			this.anomalyName = String.Copy(node.GetValue ("anomalyName"));
+			KourageousAnomalyContract.readAnomalyConfig ();
+			this.anomalyDisplayName = 
+				KourageousAnomalyContract.anomalies [targetBody.name + ":" + anomalyName].anomalyDescription;
+			KourageousTouristsAddOn.printDebug ("display name: " + anomalyDisplayName);
+			setDistance ();
+		}
+
+		protected override void OnSave (ConfigNode node)
+		{
+			base.OnSave (node);
+			node.AddValue ("anomalyName", anomalyName);
+		}
+	}
+}
+
